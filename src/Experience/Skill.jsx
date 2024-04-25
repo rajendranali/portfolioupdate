c83283601bda5402f02b8a9f8b1f7ae4f8801f8d
Rajendra Nali <rajendranali.dakshana17@gmail.com>
2024-04-25 23:15:16 +0530

    skills

---
+++ src/Experience/Skill.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { FaCode, FaTools, FaCube, FaListUl, FaDatabase } from 'react-icons/fa';
+
+const Skill = () => {
+  return (
+    <div className="max-w-4xl mx-auto py-8">
+      <h2 className="text-3xl font-bold text-center mb-8">Skills</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {/* MERN Stack */}
+        <div className="bg-white rounded-lg shadow-md p-6 flex items-center justify-center flex-col">
+          <FaCode className="text-4xl text-gray-800 mb-4" />
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">MERN Stack</h3>
+          <ul className="text-gray-600">
+            <li>JavaScript/TypeScript</li>
+            <li>HTML/CSS</li>
+            <li>React.js</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MongoDB</li>
+            {/* Add more skills as needed */}
+          </ul>
+        </div>
+        {/* Other Skills */}
+        <div className="bg-white rounded-lg shadow-md p-6 flex items-center justify-center flex-col">
+          <FaTools className="text-4xl text-gray-800 mb-4" />
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">Other Skills</h3>
+          <ul className="text-gray-600">
+            <li>C/C++</li>
+            <li>Python</li>
+            <li>Data Structures and Algorithms</li>
+            <li>Git/GitHub</li>
+            <li>GitLab</li>
+            <li>Webpack</li>
+            {/* Add more skills as needed */}
+          </ul>
+        </div>
+        {/* Frameworks */}
+        <div className="bg-white rounded-lg shadow-md p-6 flex items-center justify-center flex-col">
+          <FaCube className="text-4xl text-gray-800 mb-4" />
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">Frameworks</h3>
+          <ul className="text-gray-600">
+            <li>Material-UI</li>
+            <li>FastAPI</li>
+            <li>Django</li>
+            <li>Antd</li>
+            <li>Chakra-UI</li>
+            {/* Add more frameworks as needed */}
+          </ul>
+        </div>
+        {/* Libraries */}
+        <div className="bg-white rounded-lg shadow-md p-6 flex items-center justify-center flex-col">
+          <FaListUl className="text-4xl text-gray-800 mb-4" />
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">Libraries</h3>
+          <ul className="text-gray-600">
+            <li>Redux</li>
+            <li>Redux Toolkit</li>
+            <li>React Router</li>
+            <li>pandas</li>
+            <li>NumPy</li>
+            <li>Matplotlib</li>
+            {/* Add more libraries as needed */}
+          </ul>
+        </div>
+        {/* Database */}
+        <div className="bg-white rounded-lg shadow-md p-6 flex items-center justify-center flex-col">
+          <FaDatabase className="text-4xl text-gray-800 mb-4" />
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">Database</h3>
+          <ul className="text-gray-600">
+            <li>SQL</li>
+            <li>MySQL</li>
+            {/* Add more databases as needed */}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skill;
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import ImagesProfile from "./ImagesProfile";
 import Education from "./Education";
 import Experience from "../Experience/Experience";
+import { Link } from "react-router-dom";
+import Skill from "../Experience/Skill";
 
 const About = () => {
+  const resumeLink = "https://drive.google.com/file/d/1oHVBX5DUg8-qCc_wm955Mq0_V_rOBnTI/view?usp=drive_link";
+
   return (
     <div className="bg-gray-100 py-12">
       <div className="max-w-4xl mx-auto px-6 lg:px-0 flex flex-col lg:flex-row items-center justify-center lg:justify-between">
@@ -14,7 +18,6 @@ const About = () => {
               I'm Rajendra
             </span>
             <span className="block text-transparent bg-gradient-to-r from-red-500  to-blue-700 bg-clip-text">Software Developer</span>
-
           </h1>
 
           <p className="text-lg lg:text-xl text-gray-700 mb-4 italic tracking-wide">
@@ -22,13 +25,21 @@ const About = () => {
             solutions. Crafting elegant code and designing intuitive user
             interfaces are my specialties.
           </p>
+
+          <Link 
+            to={resumeLink} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            View Resume
+          </Link>
         </div>
         <div className="lg:w-1/2 mt-8 lg:mt-0">
           <ImagesProfile />
         </div>
       </div>
-    
-      
+      <Skill/>
     </div>
   );
 };

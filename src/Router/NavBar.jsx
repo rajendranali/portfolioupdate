2dfcd51a5bfc5d250a407877fb042dc471e30550
Rajendra Nali <rajendranali.dakshana17@gmail.com>
2024-04-25 20:07:33 +0530

    responsive

--- src/Router/NavBar.jsx
+++ src/Router/NavBar.jsx
@@ -1,39 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const NavBar = () => {
   return (
-    <nav className="text-black px-4 md:px-20 ">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center py-4">
+    <nav className="text-black">
+      <div className="container mx-auto flex justify-around items-center py-4">
         <div className="flex items-center">
-          <h1 className="text-lg font-bold italic tracking-wide">Rajendra Nali</h1>
-          <ul className="md:ml-8 flex space-x-4 hidden md:flex">
+          <h1 className="text-lg font-bold">Rajendra Nali</h1>
+          <ul className="hidden md:flex space-x-4 ml-8">
             <li>
-              <Link to="/" className="hover:text-gray-400">About</Link>
+              <Link to="/" className="hover:text-gray-400">
+                About
+              </Link>
             </li>
             <li>
-              <Link to="/experience" className="hover:text-gray-400">Experience</Link>
+              <Link to="/experience" className="hover:text-gray-400">
+                Experience
+              </Link>
             </li>
             <li>
-              <Link to="/projects" className="hover:text-gray-400">Projects</Link>
+              <Link to="/projects" className="hover:text-gray-400">
+                Projects
+              </Link>
             </li>
           </ul>
         </div>
         <div className="mt-4 md:mt-0">
           <ul className="flex space-x-4">
             <li>
-              <Link to="/github" className="hover:text-gray-400">
-                <FaGithub/>
-              </Link>
+              <a
+                href="https://github.com/rajendranali"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-400"
+              >
+                <FaGithub className="text-gray-700" />
+              </a>
             </li>
             <li>
-              <Link to="/linkedin" className="hover:text-gray-400"><FaLinkedin /></Link>
+              <a
+                href="https://www.linkedin.com/in/rajendra-nali-760762170/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-400"
+              >
+                <FaLinkedin className="text-gray-700" />
+              </a>
             </li>
             <li>
-              <Link to="/email" className="hover:text-gray-400"><MdEmail /></Link>
+              <a
+                href="mailto:rajendranali.dakshana17@gmail.com"
+                className="hover:text-gray-400"
+              >
+                <MdEmail className="text-gray-700" />
+              </a>
             </li>
           </ul>
         </div>
